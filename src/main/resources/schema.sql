c756ab6acf0071085be2b1bb25a114110676519e
3030914 <3030914@30309-14>
2024-09-20 14:17:42 +0900

    最初のコミット

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,17 @@
+create table unit(id serial primary key,
+				 name varchar(255) not null,
+				 mhp int not null,
+				 hp int not null,
+				 mmp int not null,
+				 mp int not null,
+				 str int not null,
+				 inte int not null,
+				 skl int not null,
+				 agi int not null,
+				 def int not null,
+				 mdf int not null,
+				 luck int not null )
+
+create table users(id serial primary key, 
+				 username varchar(255) not null,
+				 password varchar(255) not null )
